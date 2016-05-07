c78527e1683d5272b80a91c73e31a22db735ee05
YUSUFKILDAN <kildanyusuf@gmail.com>
2016-05-07 14:58:26 +0300

    Initial Commit

---
+++ Instagram Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram Client
+//
+//  Created by yusuf_kildan on 07/05/16.
+//  Copyright © 2016 yusuf_kildan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
