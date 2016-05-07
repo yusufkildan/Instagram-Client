917077e444ca0fda84544d07918716d77d729a0d
YUSUFKILDAN <kildanyusuf@gmail.com>
2016-05-07 15:47:04 +0300

    InstagramTableViewCell added

---
+++ Instagram Client/InstagramTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstagramTableViewCell.swift
+//  Instagram Client
+//
+//  Created by yusuf_kildan on 07/05/16.
+//  Copyright © 2016 yusuf_kildan. All rights reserved.
+//
+
+import UIKit
+
+class InstagramTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
