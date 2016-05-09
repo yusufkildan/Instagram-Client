562ef7f525b618319889609a31db6f64ab141e31
YUSUFKILDAN <kildanyusuf@gmail.com>
2016-05-10 00:45:54 +0300

    Search functionality added

--- Instagram Client/InstagramVC.swift
+++ Instagram Client/InstagramVC.swift
@@ -1,19 +1,24 @@
 //
-//  InstagramVC.swift
+//  InstagramViewController.swift
 //  Instagram Client
 //
-//  Created by yusuf_kildan on 07/05/16.
+//  Created by yusuf_kildan on 04/05/16.
 //  Copyright © 2016 yusuf_kildan. All rights reserved.
 //
 
 import UIKit
 import PureLayout
-class InstagramVC: UIViewController,UITableViewDataSource ,UITableViewDelegate {
+
+class InstagramVC: UIViewController ,UITableViewDataSource ,UITableViewDelegate ,UISearchBarDelegate {
+    
     var tableView  :UITableView!
+    var inSearchMode = false
+    var searchController : UISearchController!
+    var refreshContol : UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.whiteColor()
         let refreshButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh, target: self, action: #selector(InstagramVC.refreshButton))
         navigationItem.leftBarButtonItem = refreshButton
         self.navigationItem.title = "INSTAGRAM"
@@ -27,28 +32,95 @@ class InstagramVC: UIViewController,UITableViewDataSource ,UITableViewDelegate {
         tableView.autoPinEdgeToSuperviewEdge(.Bottom, withInset: 0)
         tableView.autoPinEdgeToSuperviewEdge(.Left, withInset: 0)
         tableView.autoPinEdgeToSuperviewEdge(.Right, withInset: 0)
-
+        tableView.registerClass(InstagramTableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchBar.delegate = self
+        searchController.searchBar.placeholder = "Enter a hastag without #"
+        searchController.searchBar.backgroundColor =  UIColor(red: 24/255.0, green: 88/255.0, blue: 135/255.0, alpha: 1.0)
+        searchController.dimsBackgroundDuringPresentation = false
+        tableView.tableHeaderView = searchController.searchBar
+        
+        
+        refreshContol = UIRefreshControl()
+        refreshContol.attributedTitle = NSAttributedString(string: "Updating!")
+        refreshContol.addTarget(self, action: #selector(InstagramVC.refreshData), forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshContol, atIndex: 0)
+        
+        INSTANCE.downloadLastPostDatas {
+            print("Downloaded")
+            self.tableView.reloadData()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-      
+    
+    func refreshData() {
+        INSTANCE.searchPosts = []
+        INSTANCE.posts = []
+        
+        if inSearchMode {
+            
+            INSTANCE.downloadSearchPostDatas(SEARCH_TEXT, completion: {
+                self.tableView.reloadData()
+                self.refreshContol.endRefreshing()
+            })
+        }else {
+            
+            INSTANCE.downloadLastPostDatas({
+                self.tableView.reloadData()
+                self.refreshContol.endRefreshing()
+            })
+            
+        }
+        self.tableView.scrollRectToVisible(CGRectMake(0, 0, 1, 1), animated: true)
     }
+    
     func refreshButton(){
         self.tableView.reloadData()
         self.tableView.scrollRectToVisible(CGRectMake(0, 0, 1, 1), animated: true)
     }
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        
+//        let photosDetailsVC = PhotoDetailVC()
+//        var url : NSURL!
+//        if inSearchMode && INSTANCE.searchPosts.count > 0 {
+//            url = NSURL(string : INSTANCE.searchPosts[indexPath.row].mainImageURL)
+//        }else if inSearchMode == false && INSTANCE.posts.count > 0{
+//            url = NSURL(string : INSTANCE.posts[indexPath.row].mainImageURL)
+//        }
+//        photosDetailsVC.imageURL = url
+//        self.navigationController?.pushViewController(photosDetailsVC, animated: true)
+//    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as? InstagramTableViewCell {
+            
+            if inSearchMode  && INSTANCE.searchPosts.count > 0 {
+                cell.configureCell(INSTANCE.searchPosts[indexPath.row])
+            }else if INSTANCE.posts.count > 0{
+                cell.configureCell(INSTANCE.posts[indexPath.row])
+            }
+            
+            cell.setNeedsUpdateConstraints()
+            cell.updateConstraintsIfNeeded()
+            
+            return cell
+        }else {
+            
+            return InstagramTableViewCell()
+        }
         
     }
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        
+        if inSearchMode {
+            return INSTANCE.searchPosts.count
+        }
+        return INSTANCE.posts.count
     }
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 350.0
@@ -56,8 +128,43 @@ class InstagramVC: UIViewController,UITableViewDataSource ,UITableViewDelegate {
     
     
     func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
-       
+        if indexPath.row == INSTANCE.posts.count - 4 {
+            ApiManager.Instance.downloadPaginationDatas({
+                tableView.reloadData()
+            })
+            
+        }
     }
     
-
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        inSearchMode = false
+        
+        INSTANCE.searchPosts = []
+        INSTANCE.posts = []
+        INSTANCE.downloadLastPostDatas {
+            self.refreshButton()
+        }
+        
+        
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        inSearchMode = true
+        if let searchText = searchBar.text {
+            SEARCH_TEXT=searchText
+            INSTANCE.searchPosts = []
+            INSTANCE.posts = []
+            if INSTANCE.downloadSearchPostDatas(searchText, completion: {self.tableView.reloadData()}) {
+                tableView.reloadData()
+                print(INSTANCE.searchPosts.count)
+            }else {
+                YKEasyAlertController.alert("Error", message: "Please enter VALID search keyword!")
+            }
+        }
+    }
+    
+    
+    
+    
 }
